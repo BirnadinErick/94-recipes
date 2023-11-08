87b5c299a09feacc497fcce77d6121ca95958210
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-11-08 10:46:01 +0530

    fix: remove TODO as security concerns are not valid anymore

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -5,9 +5,10 @@ function initDB(URI, DB_NAME) {
   const database = client.db(DB_NAME);
 
   var repo = {}; // meta container for internal mongo-lib constructs
-  repo.client = client; // the client, to close it we need to pass this around
-  // TODO: create a class and make client private,
-  // TODO: to prevent bypassing below collection and getting database
+
+  // the client, to close it we need to pass this around
+  // ok to pass, as to controllers, we only pass collection objects
+  repo.client = client;
 
   // instead of `database`, return `collection` to decrease the
   // surface exposed!
