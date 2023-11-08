41ab2c4a87e2a67eba11bb610abe5e09a09bc880
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-11-08 16:45:37 +0530

    feat: add Header.jsx

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between px-4">
+      <div className="flex justify-start items-center space-x-4">
+        <div className="flex justify-start items-center space-x-2">
+          <img className="h-12" src={logo} alt="94Recipes logo" />
+          <h2>94Recipes</h2>
+        </div>
+        <p className="text-black/30">/</p>
+        <h4>Username</h4>
+      </div>
+      <nav className="flex justify-center items-center space-x-4">
+        <ul className="flex space-x-4 items-baseline justify-end cursor-pointer select-none">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li
+            onClick={() => {
+              // programatically trigger a full page reload
+              document.location.reload();
+            }}
+          >
+            Refresh
+          </li>
+        </ul>
+
+        <button>Add new Recipe</button>
+      </nav>
+    </header>
+  );
+}
