27407112fe668bfffcdd57b93a2d1a1934c0e1f9
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-11-08 16:45:11 +0530

    feat: add basic Root skeleton

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Root from "./routes/root";
+import ErrorPage from "./components/Error";
+import SignUp from "./routes/signin";
+import signInAction from "./utils/signInAction";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/signup",
+        element: <SignUp />,
+        action: signInAction,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
