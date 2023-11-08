24676d8af98833d38dfb5bf534bc668b8dc80a94
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-11-08 10:45:02 +0530

    feat!: add users POST with only collection object

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,5 @@
+const password = require("../utils/password");
+
 /* User Controllers
  * This module lists the 4 controllers responsible for the `user` resource
  * Function names referes to the operation on the Data layer. These functions
@@ -5,41 +7,46 @@
  */
 
 function getAUser(req, res) {
-    // get uname
-    const uname = req.params.uname
-    // query db
-    
-    // parse the results
-    res.send([req.params.uname])
+  // get uname
+  const uname = req.params.uname;
+  // query db
+
+  // parse the results
+  res.send([req.params.uname]);
 }
 
-function createAUser(req, res) {
-    // get the user input
+function createAUser(usersCollection) {
+  return async (req, res) => {
     // TODO: @2nd iteration validations
     // create record
-    // return uname
-    res.send([1])
+    const docToInsert = {
+      uname: req.query.uname,
+      passwd: await password.hash(req.query.passwd),
+      pic: req.query.pic,
+      bio: req.query.bio,
+    };
+    await usersCollection.insertOne(docToInsert);
 
+    res.status(201).json({ uname: docToInsert.uname });
+  };
 }
 
 function updateAUser(req, res) {
-    // get the fields to upadte
-    // TODO: @2nd iteration validations
-    // update the fields
-    // return uname
-    res.send([1])
-
+  // get the fields to upadte
+  // TODO: @2nd iteration validations
+  // update the fields
+  // return uname
+  res.send([1]);
 }
 
 function deleteAUser(req, res) {
-    // get the uname
-    // delete the record
-    // return deleted uname
-    res.send([1])
+  // get the uname
+  // delete the record
+  // return deleted uname
+  res.send([1]);
 }
 
-
-exports.getAUser = getAUser
-exports.createAUser = createAUser
-exports.updateAUser = updateAUser
-exports.deleteAUser = deleteAUser
+exports.getAUser = getAUser;
+exports.createAUser = createAUser;
+exports.updateAUser = updateAUser;
+exports.deleteAUser = deleteAUser;
