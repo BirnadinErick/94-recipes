7ff273e32e37b200c0d88a392b2b4ac86b47f1a4
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-11-08 10:46:18 +0530

    feat: add password utility

---
+++ backend/utils/password.js
@@ -0,0 +1,22 @@
+const bcryptLib = require("bcrypt");
+
+/* Password utility
+ * This module contains all the utility functions that work on
+ * password or any secrets passed around but should not be
+ * accessible except clients with suitable keys
+ *
+ * Algorithm used is bcrypt. Recommended by NSA. To increase the
+ * strength increment the magnitude of the SALT_ROUNDS; unironically, app
+ * will take more time to generate hash as well
+ *
+ * Functions are asynchrous to mitigate somewhat Timing attacks!
+ */
+
+const SALT_ROUNDS = 5; // why 5? that's my month of birth ;)
+
+async function hashPlainText(passwd) {
+  const hash = await bcryptLib.hash(passwd, SALT_ROUNDS);
+  return hash;
+}
+
+exports.hash = hashPlainText;
