613ad0db01b62cff64d6157eb7301e54430a55b7
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-11-09 09:51:29 +0530

    chore: style recipe details

--- frontend/src/routes/recipe.jsx
+++ frontend/src/routes/recipe.jsx
@@ -3,21 +3,27 @@ import { useLoaderData } from "react-router-dom";
 export default function Recipe() {
   const { recipe } = useLoaderData();
   return (
-    <section>
-      <h1>{recipe.title}</h1>
-      <p>Prep time: {recipe.ptime}</p>
+    <section className="px-16 py-4">
+      <div className="mb-4">
+        <h1 className="text-3xl font-bold uppercase">{recipe.title}</h1>
+        <p className="text-gray-600">Prep time: {recipe.ptime}</p>
+      </div>
 
-      <div>
-        <h3>Ingredients</h3>
+      <hr />
+
+      <div className="my-4">
+        <h3 className="text-xl">Ingredients</h3>
         {recipe.ing.map((i, j) => (
-          <p key={j}>
+          <p key={j} className="text-gray-600">
             <span>{Object.keys(i)[0]}</span>:{" "}
             <span>{i[Object.keys(i)[0]]}</span>
           </p>
         ))}
       </div>
 
-      <div>{recipe.body}</div>
+      <div className="mt-12 font-serif text-lg tracking-wide leading-relaxed">
+        {recipe.body}
+      </div>
     </section>
   );
 }
